6552897c90455ce021cd616ae7e25ac0980361f7
Rajesh Subhankar <bunty2327.cse@gmail.com>
2019-11-23 23:54:18 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Parse command line arguments
+	// Create a new map from the file
+	// Create and spread aliens
+	// Let the aliens invade the map
+	// Print result
+}
